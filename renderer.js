061d44f47df1482559665eadf31ca5ce07127f1c
hzy1721 <hzy1721@qq.com>
2023-11-06 19:18:41 +0800

    feat: 基本功能

---
+++ renderer.js
@@ -0,0 +1,9 @@
+const infoElement = document.getElementById('info');
+infoElement.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+const func = async () => {
+  const response = await versions.ping();
+  console.log(response);
+};
+
+func();
